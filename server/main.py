67855f81f983e05275098732168792d2e40e0988
asdfkco <84617097+asdfkco@users.noreply.github.com>
2023-08-02 11:07:51 +0900

    ocr

--- server/main.py
+++ server/main.py
@@ -1,24 +1,53 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-# import ocr_file_opt
+from fastapi.responses import JSONResponse
+import os
+import ocr_file_opt
 from concurrent.futures import ThreadPoolExecutor
 import time
 
 app = FastAPI()
 
-# ocr = ocr_file_opt
+ocr = ocr_file_opt
 
 class Item(BaseModel):
     file_name:str
 
 @app.post("/ocr_file")
-async def ocr_send_data(item:Item):
+def ocr_send_data(item:Item):
     print(item.file_name)
-    # asdf = None
-    # start_time = time.time()
-    # with ThreadPoolExecutor() as executor:
-    #     asdf = executor.submit(ocr.dis, ocr.extracted_text).result()
-    # end_time = time.time()
-    # print(f"실행 시간: {end_time - start_time:.2f}초")
-    return item
+    print(os.path.dirname(os.getcwd())+"/tmp/"+item.file_name)
+    body_info = None
+    start_time = time.time()
+    ocr.image_size(os.path.dirname(os.getcwd())+"/tmp/"+item.file_name)
+    with ThreadPoolExecutor() as executor:
+        body_info = executor.submit(ocr.dis,executor.submit(executor.submit(ocr.data,os.path.dirname(os.getcwd())+"/tmp/"+item.file_name),item.file_name),os.path.dirname(os.getcwd())+"/tmp/"+item.file_name)
+        body_info = body_info.result()
+    end_time = time.time()
+    print(f"실행 시간: {end_time - start_time:.2f}초")
+    return body_info
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import os
+# import ocr_file_opt
+# from concurrent.futures import ThreadPoolExecutor
+# import time
+#
+# app = FastAPI()
+#
+# ocr = ocr_file_opt
+#
+# class Item(BaseModel):
+#     file_name:str
+#
+# @app.post("/ocr_file")
+# async def ocr_send_data(item:Item):
+#     print(item.file_name)
+#     asdf = None
+#     start_time = time.time()
+#     with ThreadPoolExecutor() as executor:
+#         asdf = executor.submit(ocr.dis,executor.submit(ocr.extracted_text,os.path.dirname(os.getcwd())+item.file_name),os.path.dirname(os.getcwd())+item.file_name).result()
+#     end_time = time.time()
+#     print(f"실행 시간: {end_time - start_time:.2f}초")
+#     return asdf
--- server/ocr_file_opt.py
+++ server/ocr_file_opt.py
@@ -4,12 +4,18 @@ import re
 from PIL import Image
 from google.cloud import vision
 import io
-from concurrent.futures import ThreadPoolExecutor
+# from concurrent.futures import ThreadPoolExecutor
 
 # image_path : str = input()
 
-absPath = os.path.dirname(os.getcwd())
-image_path = absPath+'/tmp/gen.png'
+# absPath = os.path.dirname(os.getcwd())
+# image_path = absPath+'/tmp/gen.png'
+
+def image_size(path):
+    im = Image.open(path)
+    width,height = im.size
+    if width != 1600 and height != 2255:
+        raise Exception('사진크기가 알맞지 않습니다')
 
 
 def extract_text_from_image(image_path):
@@ -41,21 +47,22 @@ def extract_text_from_image(image_path):
     else:
         return False
 
+def data(image_path):
+    extracted_text = extract_text_from_image(image_path)
+    return extracted_text
 
-extracted_text = extract_text_from_image(image_path)
 
-
-def dis(text):
+def dis(text,path):
     found_text = False
 
-    for text in extracted_text:
+    for text in data(path):
         # print(text.description)
         if '성장곡선' in text.description:
             print("성장곡선")
-            return sungjang(image_path)
+            return sungjang(path)
         elif '부위별근육곡선' and '부위별체지방분석' in text.description:
             print("근육곡선")
-            return gen(image_path)
+            return gen(path)
         if not found_text:
             print("인바디 검사지가 아닙니다")
             break
@@ -77,88 +84,77 @@ def crop_image(input_image_path, output_image_path, left, top, right, bottom):
 
 
 def sungjang(input_image):
-    번호에서성별 = 'data1.png'
-    체수분체중 = 'data2.png'
-    골격근 = 'data3.png'
-    체지방량 = 'data4.png'
-    BMI = 'data5.png'
-    체지방률 = 'data6.png'
-    성장점수 = 'data7.png'
-    output_image_path = 'sungjang_cliping.png'
+    번호에서성별 = '../tmp/data1.png'
+    체수분체중 = '../tmp/data2.png'
+    골격근 = '../tmp/data3.png'
+    BMI = '../tmp/data4.png'
+    체지방률 = '../tmp/data5.png'
+    성장점수 = '../tmp/data6.png'
+    output_image_path = '../tmp/sungjang_cliping.png'
     # 성장곡선 케이스
-    left = 0
-    top = 150
-    right = 1600
-    bottom = 1100
+    left, top, right, bottom = 0, 150, 1600, 1100
+
+
     crop_image(input_image, output_image_path, left, top, right, bottom)
-    번호에서성별_data: dict = detail_data('sungjang_cliping.png', 번호에서성별, 55, 25, 1560, 65)
-    체수분체중_data: dict = detail_data('sungjang_cliping.png', 체수분체중, 690, 140, 130 + 690, 140 + 250)
-    골격근_data : str = detail_data('sungjang_cliping.png', 골격근, 250, 595, 250 + 740, 595 + 42)
-    체지방량_data : str = detail_data('sungjang_cliping.png', 체지방량, 250, 660, 250 + 740, 660 + 42)
-    BMI_data : str= detail_data('sungjang_cliping.png', BMI, 250, 835, 250 + 740, 835 + 42)
-    체지방률_data : str= detail_data('sungjang_cliping.png', 체지방률, 250, 900, 250 + 740, 900 + 42)
+    번호에서성별_data: dict = detail_data(output_image_path, 번호에서성별, 55, 25, 1560, 65).split(' ')
+    체수분체중_data: dict = detail_data(output_image_path, 체수분체중, 690, 140, 130 + 690, 140 + 250).split(' ')
+    골격근_data : str = detail_data(output_image_path, 골격근, 250, 595, 250 + 740, 595 + 42)
+    BMI_data : str= detail_data(output_image_path, BMI, 250, 835, 250 + 740, 835 + 42)
+    체지방률_data : str= detail_data(output_image_path, 체지방률, 250, 900, 250 + 740, 900 + 42)
     체지방률_data : str = re.findall("(\d*\.?\d+)", 체지방률_data)
-    성장점수_data : str= detail_data('sungjang_cliping.png', 성장점수, 1170, 205, 1170 + 105, 205 + 50)
+    성장점수_data : str= detail_data(output_image_path, 성장점수, 1170, 205, 1170 + 105, 205 + 50)
     os.remove(output_image_path)
     return {
-        "신장": 번호에서성별_data[1],
-        "나이": 번호에서성별_data[2],
-        "성별": 번호에서성별_data[3],
-        "검사일시": 번호에서성별_data[4] + 번호에서성별_data[5],
-        "체수분": 체수분체중_data[0],
-        "단백질": 체수분체중_data[1],
-        "무기질": 체수분체중_data[2],
-        "체지방": 체수분체중_data[3],
-        "체중": 체수분체중_data[4],
-        "골격근": 골격근_data,
-        "체지방량": 체지방량_data,
-        "BMI": BMI_data,
-        "체지방률": 체지방률_data[0],
-        "성장점수": 성장점수_data
+        "height": 번호에서성별_data[1].replace('cm',''),
+        "ages": 번호에서성별_data[2],
+        "inspection_date": 번호에서성별_data[4] + 번호에서성별_data[5],
+        "body_water": 체수분체중_data[0],
+        "protein": 체수분체중_data[1],
+        "minerals": 체수분체중_data[2],
+        "body_fat": 체수분체중_data[3],
+        "weight": 체수분체중_data[4],
+        "skeletal_muscle_mass": 골격근_data,
+        "bmi": BMI_data,
+        "body_fat_percentage": 체지방률_data[0],
+        "inbody_score": 성장점수_data
     }
 
 
 def gen(input_image):
-    output_image_path = 'gen_cliping.png'
-    번호에서성별 = 'data1.png'
-    체수분체중 = 'data2.png'
-    골격근 = 'data3.png'
-    체지방량 = 'data4.png'
-    BMI = 'data5.png'
-    체지방률 = 'data6.png'
-    성장점수 = 'data7.png'
+    번호에서성별 = '../tmp/data1.png'
+    체수분체중 = '../tmp/data2.png'
+    골격근 = '../tmp/data3.png'
+    BMI = '../tmp/data4.png'
+    체지방률 = '../tmp/data5.png'
+    성장점수 = '../tmp/data6.png'
+    output_image_path = '../tmp/gen_cliping.png'
 
     # 근육량 분포 케이스
-    left = 0
-    top = 150
-    right = 1600
-    bottom = 1250
+    left, top, right, bottom = 0, 150, 1600, 1250
     crop_image(input_image, output_image_path, left, top, right, bottom)
 
-    번호에서성별_data: dict = detail_data('gen_cliping.png', 번호에서성별, 40, 25, 1560, 65).split(' ')
-    체수분체중_data: dict = detail_data('gen_cliping.png', 체수분체중, 685, 175, 685 + 120, 175 + 250).split(' ')
-    골격근_data: str = detail_data('gen_cliping.png', 골격근, 235, 670, 235 + 720, 670 + 42)
-    체지방량_data: str = detail_data('gen_cliping.png', 체지방량, 235, 735, 235 + 720, 735 + 42)
-    BMI_data: str = detail_data('gen_cliping.png', BMI, 235, 950, 235 + 720, 950 + 42)
-    체지방률_data: str = detail_data('gen_cliping.png', 체지방률, 235, 1010, 235 + 720, 1010 + 42)
+    번호에서성별_data: dict = detail_data(output_image_path, 번호에서성별, 40, 25, 1560, 65).split(' ')
+    체수분체중_data: dict = detail_data(output_image_path, 체수분체중, 685, 175, 685 + 120, 175 + 250).split(' ')
+    골격근_data: str = detail_data(output_image_path, 골격근, 235, 670, 235 + 720, 670 + 42)
+    BMI_data: str = detail_data(output_image_path, BMI, 235, 950, 235 + 720, 950 + 42)
+    체지방률_data: str = detail_data(output_image_path, 체지방률, 235, 1010, 235 + 720, 1010 + 42)
     체지방률_data: str = re.findall("(\d*\.?\d+)", 체지방률_data)
-    성장점수_data: str = detail_data('gen_cliping.png', 성장점수, 1175, 200, 1175 + 105, 200 + 50)
+    성장점수_data: str = detail_data(output_image_path, 성장점수, 1175, 200, 1175 + 105, 200 + 50)
     os.remove(output_image_path)
     return {
-        "신장": 번호에서성별_data[1],
-        "나이": 번호에서성별_data[2],
-        "성별": 번호에서성별_data[3],
-        "검사일시": 번호에서성별_data[4] + 번호에서성별_data[5],
-        "체수분" : 체수분체중_data[0],
-        "단백질": 체수분체중_data[1],
-        "무기질": 체수분체중_data[2],
-        "체지방": 체수분체중_data[3],
-        "체중": 체수분체중_data[4],
-        "골격근": 골격근_data,
-        "체지방량": 체지방량_data,
-        "BMI": BMI_data,
-        "체지방률": 체지방률_data[0],
-        "성장점수": 성장점수_data
+        "height": 번호에서성별_data[1].replace('cm',''),
+        "ages": 번호에서성별_data[2],
+        "inspection_date": 번호에서성별_data[4] + 번호에서성별_data[5],
+        "body_water": 체수분체중_data[0],
+        "protein": 체수분체중_data[1],
+        "minerals": 체수분체중_data[2],
+        "body_fat": 체수분체중_data[3],
+        "weight": 체수분체중_data[4],
+        "skeletal_muscle_mass": 골격근_data,
+        "bmi": BMI_data,
+        "body_fat_percentage": 체지방률_data[0],
+        "inbody_score": 성장점수_data
+
     }
 
 
@@ -169,13 +165,3 @@ def detail_data(input_image, output_image_path, left, top, right, bottom):
     print(data_body)
     return data_body
 
-
-def start(path):
-    start_time = time.time()
-
-    # ThreadPoolExecutor 사용하여 각 이미지 처리를 병렬로 실행
-    with ThreadPoolExecutor() as executor:
-        executor.submit(dis, extracted_text)
-    end_time = time.time()
-    print(f"실행 시간: {end_time - start_time:.2f}초")
-#    json 가공
